7c287230608aaea5dc784bed7370a6877a4048ba
joabest <114368227+joabest@users.noreply.github.com>
2026-09-18 08:20:38 -0300

    Make activity bar full width above sidebar

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -9,22 +9,26 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
   const [mobileOpen, setMobileOpen] = useState(false);
 
   return (
-    <div className="flex min-h-screen bg-bg">
-      <Sidebar />
-      {mobileOpen && (
-        <>
-          <div
-            className="fixed inset-0 z-40 bg-black/35 lg:hidden"
-            onClick={() => setMobileOpen(false)}
-          />
-          <Sidebar mobile onClose={() => setMobileOpen(false)} />
-        </>
-      )}
+    <div className="min-h-screen bg-bg">
+      <ActivityTicker />
 
-      <div className="flex-1 min-w-0">
-        <ActivityTicker />
-        <Header onMenu={() => setMobileOpen(true)} />
-        {children}
+      <div className="flex min-h-[calc(100vh-36px)]">
+        <Sidebar />
+
+        {mobileOpen && (
+          <>
+            <div
+              className="fixed inset-x-0 bottom-0 top-[36px] z-40 bg-black/35 lg:hidden"
+              onClick={() => setMobileOpen(false)}
+            />
+            <Sidebar mobile onClose={() => setMobileOpen(false)} />
+          </>
+        )}
+
+        <div className="flex-1 min-w-0">
+          <Header onMenu={() => setMobileOpen(true)} />
+          {children}
+        </div>
       </div>
     </div>
   );
